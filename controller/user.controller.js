3e8a76813949a716eeb7b8337da8fa81549fb527
Shahzad <mech1090@gmail.com>
2022-06-28 09:12:52 -0600

    controller created

---
+++ controller/user.controller.js
@@ -0,0 +1,9 @@
+
+
+
+const getLoginForm =(req,res)=>{}
+const login =(req,res)=>{}
+const getSignup =(req,res)=>{}
+const signup =(req,res)=>{}
+
+module.exports = {getLoginForm,login,getSignup,signup}
